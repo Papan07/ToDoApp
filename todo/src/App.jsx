c55fb09c1a3e3eed575990fc75c50f9e3d789494
Papan07 <papanns19@gmail.com>
2025-02-08 16:39:53 +0530

    todo

---
+++ todo/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      description:"Learn react",
+      status:true
+    }
+  ])
+
+  const [todoDescription,setTododescription]=useState("");
+  function handleInput(e){
+    setTodoDescription({
+      description:e.target.value,
+      status:false
+    })
+  }
+
+  function createTodo(){
+    const tempTodoList=[...todos];
+    tempTodoList.unshift(todoDescription);
+    setTodos(tempTodoList)
+  }
+  return (
+    <>
+    <div>
+      <input type="text" onChange={handleInput} placeholder='Write your Todo' />
+      <button onClick={createTodo}>Add ToDo</button>
+    </div>
+    <div>
+      <h1>To-Do List</h1>
+      {
+        todos.map((ele)=>{
+          return  <div>
+            <span>Description.{ele.description}{"\n"}</span>
+            <span>Status: {ele.status?"Done":"Pending"}</span>
+          </div>
+        })
+      }
+    </div>
+    </>
+  )
+}
+
+export default App
